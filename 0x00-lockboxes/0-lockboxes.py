fbea541a2a3abbbd3a042271da2a29ab33544cf5
Sebastian Lopez H <sebaslhdev@gmail.com>
2021-04-22 17:11:57 +0000

    Lockboxes exercise

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """
+    You have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1 and
+    each box may contain keys to the other boxes.
+
+    Write a method that determines if all the boxes can be opened.
+
+    Prototype: def canUnlockAll(boxes)
+    boxes is a list of lists
+    A key with the same number as a box opens that box
+    You can assume all keys will be positive integers
+    There can be keys that do not have boxes
+    The first box boxes[0] is unlocked
+    Return True if all boxes can be opened, else return False
+    """
+
+    n_boxes = len(boxes)
+    if not boxes or n_boxes <= 0:
+        return False
+
+    initial_keys = [0]
+
+    for key in initial_keys:
+        for k_box in boxes[key]:
+            if 0 < k_box < n_boxes and k_box not in initial_keys:
+                initial_keys.append(k_box)
+
+    if len(initial_keys) == n_boxes:
+        return True
+
+    return False
---
+++ 0x00-lockboxes/README.md
@@ -0,0 +1,14 @@
+# 0x00. Lockboxes
+
+## Resources:books:
+Read or watch:
+
+---
+## Learning Objectives:bulb:
+What you should learn from this project:
+
+---
+---
+
+## Author
+* **Sebastian Lopez Herrera** - [sebaslherrera](https://github.com/sebaslherrera)
