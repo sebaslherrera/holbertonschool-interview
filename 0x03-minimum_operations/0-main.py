25241be92490adfdf4cbd391861f34e0443f7ec4
Sebastian Lopez H <sebaslhdev@gmail.com>
2021-08-22 23:09:41 -0500

    Use greatest common divisor to calcules min operations

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -10,3 +10,6 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 2147483640
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,6 +4,20 @@
     """
 
 
+def gcd(num):
+    """Calc the greatest common divisor
+    Returns:
+        [int]: greatest common divisor
+    """
+    if not isinstance(num, int) or num <= 1:
+        return 0
+    if num == 2:
+        return 1
+    for divisor in range(num // 2 + 1, 0, -1):
+        if num % divisor == 0:
+            return divisor
+
+
 def minOperations(n):
     """Given a number n, write a method that calculates the fewest number
     of operations needed to result in exactly n H characters in the file
@@ -19,11 +33,15 @@ def minOperations(n):
         return 0
 
     ans = 0
+    gcds = []
 
-    for i in range(2, n + 1):
-        while n % i == 0:
-            ans += i
-            # smaller problem
-            n = n / i
+    while n > 1:
+        gcds.append(int(gcd(n)))
+        n = gcds[-1]
+
+    for i in range(len(gcds)):
+        if i == len(gcds) - 1:
+            break
+        ans += gcds[i] // gcds[i + 1]
 
     return ans
