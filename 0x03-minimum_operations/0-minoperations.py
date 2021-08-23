2d3ebd983cbf3f81b00876fe16739a4d39557fcd
Sebastian Lopez H <sebaslhdev@gmail.com>
2021-08-22 22:07:58 -0500

    Added answer

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+"""0x03. Minimum Operations
+    """
+
+
+def minOperations(n):
+    """Given a number n, write a method that calculates the fewest number
+    of operations needed to result in exactly n H characters in the file
+
+    Args:
+        n (int): represents the number of 'H'
+
+    Returns:
+        int: minimum copy and paste operations
+    """
+
+    if n <= 1:
+        return 0
+
+    ans = 0
+
+    for i in range(2, n + 1):
+        while n % i == 0:
+            ans += i
+            # smaller problem
+            n = n / i
+
+    return ans
---
+++ 0x03-minimum_operations/README.md
@@ -0,0 +1,10 @@
+# 0x03. Minimum Operations
+
+## Resources:books:
+Read or watch:
+
+
+---
+
+## Author
+* **Sebastian Lopez Herrera** - [sebaslherrera](https://github.com/sebaslherrera)
